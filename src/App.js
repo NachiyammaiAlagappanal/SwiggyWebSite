a71d2aed6f69b09c06efab992d40b5450c2e0670
Nachu <nachiyammai.al@gmail.com>
2025-04-24 20:48:18 +0530

    Added example for api calls.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import Header from './Header/Header'
 import Body from "./Body/Body";
 import Footer from "./Footer/Footer";
+import { useEffect } from "react";
 import './App.css'
+import getData from "./Data";
 
 const App = () => 
-   <div>
-        <Header/>
-        <Body/>
-        <Footer/>
-    </div>
+   {
+
+  useEffect(() => {
+    getData();
+  }, []);
+        return <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>;
+    }
 
 
 export default App;
